21708b02997d5eb50a0db96bbcdea695376d9a47
Harshwardhan Nalawade <harshwardhann@gmail.com>
2024-11-14 16:04:08 +0000

    add credentials

--- Task2/deploy.sh
+++ Task2/deploy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-# remove running containers
 docker rm -f $(docker ps -qa) || sleep 1
-# create a network
 docker network create trio-task-network || sleep 1
-#create a volume
-docker volume create new-volume || sleep 1 
-# build flask and mysql
+docker volume create new-volume || sleep 1
 docker build -t trio-task-mysql:5.7 db
 docker build -t trio-task-flask-app:latest flask-app
-# run mysql container
+
 docker run -d \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
     --name mysql \
     --network trio-task-network \
     trio-task-mysql:5.7
-# run flask container
+
 docker run -d \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
     --name flask-app \
     --network trio-task-network \
     trio-task-flask-app:latest
-# run the nginx container
+
 docker run -d \
     --name nginx \
     -p 80:80 \
     --network trio-task-network \
     --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
     nginx:latest
-# show running containers
+
+sleep 5
 docker ps -a
